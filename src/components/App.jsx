b333bc8478bd96b7d821158a1fbf184c1e86e37b
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-11-19 21:42:02 +0200

    01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,22 @@ export class App extends Component {
     filter: '',
   };
 
+  // Під час завантаження застосунку контакти, якщо такі є, зчитуються з локального сховища і записуються у стан.
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  // Під час додавання та видалення контакту контакти зберігаються у локальне сховище.
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = data => {
     const { contacts } = this.state;
 
